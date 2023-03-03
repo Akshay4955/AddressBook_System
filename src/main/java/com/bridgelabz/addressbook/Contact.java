c9f69eb13b0a115f455950aa6ce7a0eae96520c6
Akshay4955 <akshayshedge11@gmail.com>
2023-03-03 15:34:53 +0530

    Added:Code to create contacts in AddressBook

---
+++ src/main/java/com/bridgelabz/addressbook/Contact.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.addressbook;
+
+public class Contact {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    int zipCode;
+    long phoneNumber;
+    String email;
+}
