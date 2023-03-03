a2d8c6fe1ce40d65685982805ff66b3e3c4504bb
Akshay4955 <akshayshedge11@gmail.com>
2023-03-03 16:16:13 +0530

    Added:Code to edit existing contact

--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -4,11 +4,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBookMain {
+    Scanner input = new Scanner(System.in);
     ArrayList<Contact> contacts = new ArrayList<>();
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook program....!!!!");
         AddressBookMain addressBook = new AddressBookMain();
         addressBook.addNewContacts();
+        System.out.println("Before Editing Contacts: ");
+        for (Contact x : addressBook.contacts) {
+            System.out.println(x.firstName + " " + x.lastName + " " + x.address + " " + x.city
+                    + " " + x.state + " " + x.zipCode + " " + x.phoneNumber + " " + x.email);
+        }
+        addressBook.editContact();
+        System.out.println("After Editing Contacts: ");
         for (Contact x : addressBook.contacts) {
             System.out.println(x.firstName + " " + x.lastName + " " + x.address + " " + x.city
             + " " + x.state + " " + x.zipCode + " " + x.phoneNumber + " " + x.email);
@@ -17,7 +25,6 @@ public class AddressBookMain {
 
     public void addNewContacts() {
         System.out.println("Enter how many contacts you want to save");
-        Scanner input = new Scanner(System.in);
         int noOfContacts = input.nextInt();
 
         for (int i = 0; i < noOfContacts; i++) {
@@ -40,6 +47,58 @@ public class AddressBookMain {
             contact.email = input.next();
 
             contacts.add(contact);
+        }
+    }
+
+    public void editContact() {
+        System.out.println("Please Enter the name whose details you want to change");
+        String name = input.next();
+        for (int i = 0; i < contacts.size(); i++) {
+            if (name.equalsIgnoreCase(contacts.get(i).firstName)) {
+                System.out.println("What you want to change :" + '\n' + "Press 1 for first name " + '\n' +
+                        "Press 2 for last name " + '\n' + "Enter 3 for address " + '\n' + "Enter 4 for city"
+                + '\n' + "Enter 5 for state" + '\n' + "Enter 6 for zip code" + '\n' + "Enter 7 for phone number"
+                + '\n' + "Enter 8 for email");
+                int choice = input.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("Please Enter the name to be updated");
+                        contacts.get(i).firstName = input.next();
+                        break;
+                    case 2:
+                        System.out.println("Please Enter the lastname to be updated");
+                        contacts.get(i).lastName = input.next();
+                        break;
+                    case 3:
+                        System.out.println("Please Enter the address to be updated");
+                        contacts.get(i).address = input.next();
+                        break;
+                    case 4:
+                        System.out.println("Please Enter the city to be updated");
+                        contacts.get(i).city = input.next();
+                        break;
+                    case 5:
+                        System.out.println("Please enter the state to be updated");
+                        contacts.get(i).state = input.next();
+                        break;
+                    case 6:
+                        System.out.println("Please Enter the zip code to be updated");
+                        contacts.get(i).zipCode = input.nextInt();
+                        break;
+                    case 7:
+                        System.out.println("Please Enter the phone number to be updated");
+                        contacts.get(i).phoneNumber = input.nextLong();
+                        break;
+                    case 8:
+                        System.out.println("Please Enter the email to be updated");
+                        contacts.get(i).email = input.next();
+                        break;
+                    default:
+                        System.out.println("You Entered wrong input");
+
+                }
+
+            }
         }
     }
 }
