bb04e954f03b2ea680d08cc26d130cebcc690fa2
Akshay4955 <akshayshedge11@gmail.com>
2023-03-23 19:04:52 +0530

    Added: Code to get sorted addressbook by name

--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -19,8 +19,9 @@ public class AddressBookMain {
             System.out.println("Enter what you want to perform");
             System.out.println("Press 1 to create new address book" + '\n' + "Press 2 to perform operation " +
                     "on existing address book" + '\n' + "Press 3 to search contacts with city " + '\n' + "Press 4 to get person with city" +
-                    '\n' + "Press 5 to get number of contacts by city" + '\n' + "Press 0 to exit");
-            final int createAddressBook = 1, operateExisting = 2, searchContacts = 3, getPersonWithCity = 4, getNoOfContactByCity = 5, exit = 0;
+                    '\n' + "Press 5 to get number of contacts by city" + '\n' + "Press 6 to get sorted contacts by name" + '\n' + "Press 0 to exit");
+            final int createAddressBook = 1, operateExisting = 2, searchContacts = 3, getPersonWithCity = 4, getNoOfContactByCity = 5,
+                      sortedContactsByName = 6, exit = 0;
             int option = input.nextInt();
             switch (option) {
                 case createAddressBook:
@@ -42,6 +43,9 @@ public class AddressBookMain {
                     break;
                 case getNoOfContactByCity:
                     addressBookMain.getNumberContacts();
+                    break;
+                case sortedContactsByName:
+                    addressBookMain.getSortedContactByName();
                     break;
                 case exit:
                     loop = false;
@@ -66,7 +70,8 @@ public class AddressBookMain {
     public void searchContactsWithCity() {
         System.out.println("Please enter city name");
         String cityName = input.next();
-        List<Contact> listOfContacts = addressBooks.values().stream().flatMap(p -> p.stream()).filter(p -> p.getCity().equalsIgnoreCase(cityName)).collect(Collectors.toList());
+        List<Contact> listOfContacts = addressBooks.values().stream().flatMap(p -> p.stream())
+                                       .filter(p -> p.getCity().equalsIgnoreCase(cityName)).collect(Collectors.toList());
         System.out.println(listOfContacts);
     }
 
@@ -85,4 +90,9 @@ public class AddressBookMain {
         System.out.println("Count of contacts with " + cityName + " are " + count);
     }
 
+    public void getSortedContactByName() {
+        List<Contact> nameSortedAddressBook = addressBooks.values().stream().flatMap(A -> A.stream())
+                .sorted((Comparator.comparing(Contact::getFirstName))).collect(Collectors.toList());
+        System.out.println(nameSortedAddressBook);
+    }
 }
