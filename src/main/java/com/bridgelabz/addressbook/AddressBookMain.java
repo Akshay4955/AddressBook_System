02a57ee42c8a625525e3d620a3465b94a54adb80
Akshay4955 <akshayshedge11@gmail.com>
2023-03-25 11:19:23 +0530

    Added: Code to sort by city, State Or Zipcode

--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -6,11 +6,8 @@ import java.util.stream.Collectors;
 public class AddressBookMain {
     HashMap<String, ArrayList<Contact>> addressBooks = new HashMap<>();
 
-    Scanner input = new Scanner(System.in);
-
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook program....!!!!");
-        Scanner input = new Scanner(System.in);
         AddressBookMain addressBookMain = new AddressBookMain();
         AddressBook addressBook = new AddressBook();
         addressBookMain.createAddressBook();
@@ -19,39 +16,44 @@ public class AddressBookMain {
             System.out.println("Enter what you want to perform");
             System.out.println("Press 1 to create new address book" + '\n' + "Press 2 to perform operation " +
                     "on existing address book" + '\n' + "Press 3 to search contacts with city " + '\n' + "Press 4 to get person with city" +
-                    '\n' + "Press 5 to get number of contacts by city" + '\n' + "Press 6 to get sorted contacts by name" + '\n' + "Press 0 to exit");
+                    '\n' + "Press 5 to get number of contacts by city" + '\n' + "Press 6 to get sorted contacts by name/City/State/Zip" + '\n' + "Press 0 to exit");
             final int createAddressBook = 1, operateExisting = 2, searchContacts = 3, getPersonWithCity = 4, getNoOfContactByCity = 5,
-                      sortedContactsByName = 6, exit = 0;
-            int option = input.nextInt();
-            switch (option) {
-                case createAddressBook:
-                    addressBookMain.createAddressBook();
-                    break;
-                case operateExisting:
-                    System.out.println("Plz enter key belong to address book");
-                    String inputKey = input.next();
-                    if (addressBookMain.addressBooks.containsKey(inputKey))
-                        addressBook.callAddressBook(addressBookMain.addressBooks.get(inputKey));
-                    else
-                        System.out.println("Entered key address book not available");
-                    break;
-                case searchContacts:
-                    addressBookMain.searchContactsWithCity();
-                    break;
-                case getPersonWithCity:
-                    addressBookMain.getContactByCityAndState();
-                    break;
-                case getNoOfContactByCity:
-                    addressBookMain.getNumberContacts();
-                    break;
-                case sortedContactsByName:
-                    addressBookMain.getSortedContactByName();
-                    break;
-                case exit:
-                    loop = false;
-                    break;
-                default:
-                    System.out.println("Entered Wrong input");
+                      getSortedContacts = 6, exit = 0;
+            try {
+                Scanner input = new Scanner(System.in);
+                int option = input.nextInt();
+                switch (option) {
+                    case createAddressBook:
+                        addressBookMain.createAddressBook();
+                        break;
+                    case operateExisting:
+                        System.out.println("Plz enter key belong to address book");
+                        String inputKey = input.next().toLowerCase();
+                        if (addressBookMain.addressBooks.containsKey(inputKey))
+                            addressBook.callAddressBook(addressBookMain.addressBooks.get(inputKey));
+                        else
+                            System.out.println("Entered key address book not available");
+                        break;
+                    case searchContacts:
+                        addressBookMain.searchContactsWithCity();
+                        break;
+                    case getPersonWithCity:
+                        addressBookMain.getContactByCityAndState();
+                        break;
+                    case getNoOfContactByCity:
+                        addressBookMain.getNumberContacts();
+                        break;
+                    case getSortedContacts:
+                        addressBookMain.getSortedContacts();
+                        break;
+                    case exit:
+                        loop = false;
+                        break;
+                    default:
+                        System.out.println("Entered Wrong input");
+                }
+            }catch (InputMismatchException e) {
+                System.out.println("You entered wrong input. Please enter valid input");
             }
         }
     }
@@ -60,7 +62,8 @@ public class AddressBookMain {
         System.out.println("Create address book of your choice");
         ArrayList<Contact> contacts = new ArrayList<>();
         System.out.println("Enter unique key name");
-        String name = input.next();
+        Scanner input = new Scanner(System.in);
+        String name = input.next().toLowerCase();
         if (!addressBooks.containsKey(name))
             addressBooks.put(name, contacts);
         else
@@ -69,14 +72,15 @@ public class AddressBookMain {
 
     public void searchContactsWithCity() {
         System.out.println("Please enter city name");
+        Scanner input = new Scanner(System.in);
         String cityName = input.next();
-        List<Contact> listOfContacts = addressBooks.values().stream().flatMap(p -> p.stream())
+        List<Contact> listOfContacts = addressBooks.values().stream().flatMap(Collection::stream)
                                        .filter(p -> p.getCity().equalsIgnoreCase(cityName)).collect(Collectors.toList());
         System.out.println(listOfContacts);
     }
 
     public void getContactByCityAndState() {
-        List<Contact> myContactList = addressBooks.values().stream().flatMap(A -> A.stream()).collect(Collectors.toList());
+        List<Contact> myContactList = addressBooks.values().stream().flatMap(Collection::stream).collect(Collectors.toList());
         Map<String, List<Contact>> myContactListByCity = myContactList.stream().collect(Collectors.groupingBy(Contact::getCity));
         System.out.println(myContactListByCity);
         Map<String, List<Contact>> myContactListByState = myContactList.stream().collect(Collectors.groupingBy(Contact::getState));
@@ -84,15 +88,63 @@ public class AddressBookMain {
     }
 
     public void getNumberContacts() {
-        System.out.println("Please enter city name");
-        String cityName = input.next();
-        long count = addressBooks.values().stream().flatMap(p -> p.stream()).filter(p -> p.getCity().equalsIgnoreCase(cityName)).count();
-        System.out.println("Count of contacts with " + cityName + " are " + count);
+        System.out.println("Please enter choice parameter ");
+        System.out.println("Press 1 for City" + '\n' + "Press 2 for State");
+        final int byCity = 1, byState = 2;
+        try {
+            Scanner input = new Scanner(System.in);
+            int choice = input.nextInt();
+            switch (choice) {
+                case byCity:
+                    System.out.println("Please enter city name");
+                    String cityName = input.next();
+                    long countByCity = addressBooks.values().stream().flatMap(Collection::stream).filter(p -> p.getCity().equalsIgnoreCase(cityName)).count();
+                    System.out.println("Count of contacts with " + cityName + " are " + countByCity);
+                    break;
+                case byState:
+                    System.out.println("Please enter State name");
+                    String stateName = input.next();
+                    long countByState = addressBooks.values().stream().flatMap(Collection::stream).filter(p -> p.getCity().equalsIgnoreCase(stateName)).count();
+                    System.out.println("Count of contacts with " + stateName + " are " + countByState);
+                    break;
+                default:
+                    System.out.println("You entered wrong input");
+            }
+        }catch (InputMismatchException e) {
+            System.out.println("You entered wrong input. Please enter valid input");
+        }
+
     }
 
-    public void getSortedContactByName() {
-        List<Contact> nameSortedAddressBook = addressBooks.values().stream().flatMap(A -> A.stream())
-                .sorted((Comparator.comparing(Contact::getFirstName))).collect(Collectors.toList());
-        System.out.println(nameSortedAddressBook);
+    public void getSortedContacts() {
+        System.out.println("Please enter the choice parameter by which you want sort");
+        System.out.println("Press 1 for Name" + '\n' + "Press 2 for City" + '\n' + "Press 3 for State" + '\n' + "Press 4 for ZipCode");
+        final int byName = 1, byCity = 2, byState = 3, byZipCode = 4;
+        try {
+            Scanner input = new Scanner(System.in);
+            int choice = input.nextInt();
+            switch (choice) {
+                case byName:
+                    System.out.println(addressBooks.values().stream().flatMap(Collection::stream)
+                            .sorted((Comparator.comparing(Contact::getFirstName))).collect(Collectors.toList()));
+                    break;
+                case byCity:
+                    System.out.println(addressBooks.values().stream().flatMap(Collection::stream)
+                            .sorted((Comparator.comparing(Contact::getCity))).collect(Collectors.toList()));
+                    break;
+                case byState:
+                    System.out.println(addressBooks.values().stream().flatMap(Collection::stream)
+                            .sorted((Comparator.comparing(Contact::getState))).collect(Collectors.toList()));
+                    break;
+                case byZipCode:
+                    System.out.println(addressBooks.values().stream().flatMap(Collection::stream)
+                            .sorted((Comparator.comparing(Contact::getZipCode))).collect(Collectors.toList()));
+                    break;
+                default:
+                    System.out.println("You entered wrong input");
+            }
+        }catch (InputMismatchException e) {
+            System.out.println("You entered wrong input. Please enter valid input");
+        }
     }
 }
